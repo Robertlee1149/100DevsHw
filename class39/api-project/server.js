0527a55ab607e47ceda0cd9ea7fdcac95b1b451b
Robert Lee <robertlee1149@gmail.com>
2024-06-13 15:11:35 -0700

    Rapper Name API

---
+++ class39/api-project/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'Unknown',
+        'birthLocation': 'Unknown'
+
+    }
+
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+
+})
+
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        res.json(rappers[rapperName])
+    }else{
+        res.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`The server is now running on port ${PORT}! Betta go catch it`)
+})
